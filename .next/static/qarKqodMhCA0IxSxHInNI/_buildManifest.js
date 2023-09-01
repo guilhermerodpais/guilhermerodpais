eafa5a7eef7c351888d6741a5181b3e604067dc7
guilherme.pais <guilherme.pais@seidor.com.br>
2023-09-01 13:51:39 -0300

    Add brackets to if  condition github

---
+++ .next/static/qarKqodMhCA0IxSxHInNI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/d978e52f82c6fe65.css","static/chunks/pages/index-c9ee9c8a703639ff.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
